3429044d3bb6cc756c966211ed319ce3f4483979
Allison Carnwath <3651849+acarnwath@users.noreply.github.com>
2021-03-05 14:02:26 -0500

    minimal reproduction for https://github.com/gatsbyjs/gatsby/issues/29679

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,35 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `The site's name`,
+      short_name: `short`,
+      start_url: `/`,
+      background_color: `#ff0000`,
+      theme_color: `#ff0000`,
+      display: `minimal-ui`,
+      icon: `src/images/favicon.png`, // This path is relative to the root of the pre-rendered site
+      icons: [ // Paths relative to the static folder
+        {
+          src: `favicon/favicon-72.png`,
+          sizes: `72x72`,
+          type: `image/png`,
+        },{
+          src: `favicon/favicon-32.png`,
+          sizes: `32x32`,
+          type: `image/png`,
+        },{
+          src: `favicon/favicon-maskable.png`,
+          sizes: `310x310`,
+          type: `image/png`,
+          purpose: `maskable`,
+        },
+      ],
+      cache_busting_mode: 'none',
+    },
+  },
+  ],
 }
